54bd4f2d4952cb658c7ebd7340dc6f246cf873ff
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-02 23:52:27 -0700

    seeds a sample trip with locations and transportations

--- travel-journal-api/db/seeds.rb
+++ travel-journal-api/db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+trip = Trip.create(title: "My Philippines Adventure")
+
+trip.locations << Location.create(name: "Manila", timezone: "PHT")
+trip.locations << Location.create(name: "Banaue", timezone: "PHT")
+trip.locations << Location.create(name: "Panglau", timezone: "PHT")
+
+trip.transportations << Transportation.create(mode: "bus", start_location: "Manila", end_location: "Banaue")
+trip.transportations << Transportation.create(mode: "ferry", start_location: "Banaue", end_location: "Panlau")
