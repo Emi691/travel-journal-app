1fcad6b328b72f86551d70ecc1fc3df2c80fae71
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-05 15:47:04 -0700

    creates Location JS class with constructor

---
+++ frontend/location.js
@@ -0,0 +1,8 @@
+class Location {
+    constructor(location) {
+        this.name = location.name
+        this.timezone = location.timezone
+        this.arrival_date = location.arrival_date
+        this.departure_date = location.departure_date
+    }
+}
