523b97532df41161bd5469ad4950451f500439aa
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-05 15:50:54 -0700

    creates Transportation JS class with constructor

---
+++ frontend/transportation.js
@@ -0,0 +1,9 @@
+class Transportaion {
+    constructor(transportation) {
+        this.start_location = transportation.start_location
+        this.end_location = transportation.end_location
+        this.mode = transportation.mode
+        this.departure_date = transportation.departure_date
+        this.arrival_date = transportation.arrival_date
+    }
+}
